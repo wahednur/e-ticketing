e234f6406f5f17f57aa532d94274020b49e3ecbc
Wahed Nur <wahednur@gmail.com>
2024-02-18 01:46:43 +0600

    e-Ticket project created and banner complte

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      "e-black": "#030712",
+      "e-green": "#1DD100",
+    },
+  },
+  plugins: [],
+};
